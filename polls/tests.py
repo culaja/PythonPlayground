cee15a58589c3d82a98a1ecd7c6dbb2f231f3060
Stanko Culaja <stanko@Stankos-MacBook-Pro.local>
2017-03-29 14:55:16 +0200

    Added few test cases

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Question, Choice
+
+class QuestionMethodTests(TestCase):
+    def test_client(self):
+        resp = self.client.get('/polls/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_get_wining_choice_works(self):
+        q = Question.objects.create(question_text='This is my text', pub_date=timezone.now())
+        c = Choice.objects.create(question=q, choice_text='Simple text', votes=5)
+        self.assertIs(q.get_wining_choice().votes, 5)
+
+class QuestionDatabaseTests(TestCase):
+    fixtures = ['polls_views_testdata.json']
+    def test_something(self):
+        c = Choice.objects.all()
+        self.assertIs(len(c), 3)
+
+    def test_index_page(self):
+        resp = self.client.get('/polls/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('latest_question_list' in resp.context)
